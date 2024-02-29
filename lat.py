515ce57701c0a288df41f93aa4ea3f3f240a722c
exghost07 <ghostraider039@gmail.com>
2024-02-29 14:19:01 +0900

    r2

---
+++ lat.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+st.write("Labra24")
+df = pd.DataFrame({
+  'No': [1, 2, 3, 4],
+  'Nama': ['Asep','Alul','Hansen','viny'],
+  'Nilai': [95, 97, 98, 98]
+})
+
+df
