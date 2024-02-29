4eea4c7ee181711f8e1584bf26693f446460651e
exghost07 <ghostraider039@gmail.com>
2024-02-29 14:26:45 +0900

    r4

--- lat.py
+++ lat.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 # import matplotlib.pyplot as plt
-st.write("Labra24")
+st.write("Labra24 rev")
 df = pd.DataFrame({
   'No': [1, 2, 3, 4],
   'Nama': ['Asep','Alul','Hansen','viny'],
